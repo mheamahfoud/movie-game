8b831695348391759b61fc4e04b61413e58ffe71
AiEngineerSy <90108678+AiEngineerSy@users.noreply.github.com>
2022-01-06 07:54:53 +0200

    Updates

---
+++ precache-manifest.f529ec28ddf4be1f54f824cabbad7d18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57b0d68c403729a409ec6bb64a982d8b",
+    "url": "/example-react-app/index.html"
+  },
+  {
+    "revision": "56a07ebaf6fa0fd8a3cc",
+    "url": "/example-react-app/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "fad99e7eb36c0fb83c01",
+    "url": "/example-react-app/static/css/main.d1b8668b.chunk.css"
+  },
+  {
+    "revision": "56a07ebaf6fa0fd8a3cc",
+    "url": "/example-react-app/static/js/2.c23f0192.chunk.js"
+  },
+  {
+    "revision": "fad99e7eb36c0fb83c01",
+    "url": "/example-react-app/static/js/main.8f546e0d.chunk.js"
+  },
+  {
+    "revision": "5af9d6412488cb4cde34",
+    "url": "/example-react-app/static/js/runtime~main.2df7deb2.js"
+  }
+]);
